9f4c88db8be56715acb2ded929fc2e2ed2535310
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-09-01 07:17:14 +0600

    feat: refactor neural network initialization and implement forward pass

--- model.py
+++ model.py
@@ -1,24 +1,24 @@
 import cupy as cp
 
 class nn:
-    def __init__(self, x, z1, z2, y, learning_rate=0.01):
-        self.x = x
-        self.z1 = z1
-        self.z2 = z2
-        self.y = y
+    def __init__(self, xs, z1s, z2s, ys, learning_rate=0.01):
+        self.xs = xs
+        self.z1s = z1s
+        self.z2s = z2s
+        self.ys = ys
         self.learning_rate = learning_rate
 
         # Input to hidden layer 1
-        self.W1 = cp.random.randn(self.z1, self.x) * 0.1
-        self.b1 = cp.zeros((self.z1, 1))
+        self.W1 = cp.random.randn(self.z1s, self.xs) * 0.1
+        self.b1 = cp.zeros((self.z1s, 1))
 
         # Hidden layer 1 to hidden layer 2
-        self.W2 = cp.random.randn(self.z2, self.z1) * 0.1
-        self.b2 = cp.zeros((self.z2, 1))
+        self.W2 = cp.random.randn(self.z2s, self.z1s) * 0.1
+        self.b2 = cp.zeros((self.z2s, 1))
 
         # Hidden layer 2 to output layer
-        self.W3 = cp.random.randn(self.y, self.z2) * 0.1
-        self.b3 = cp.zeros((self.y, 1))
+        self.W3 = cp.random.randn(self.ys, self.z2s) * 0.1
+        self.b3 = cp.zeros((self.ys, 1))
 
     # Load dataset
     def load_data(self, file_path):
@@ -35,7 +35,19 @@ class nn:
 
     # forward pass, loss calculation and backpropagation
     def forwardpass(self, x):
-        pass
+        # input to hidden layer 1
+        self.z1 = self.W1 @ x + self.b1
+        self.a1 = self.relu(self.z1)
+
+        # hidden layer 1 to hidden layer 2
+        self.z2 = self.W2 @ self.a1 + self.b2
+        self.a2 = self.relu(self.z2)
+
+        # hidden layer 2 to output layer
+        self.z3 = self.W3 @ self.a2 + self.b3
+        self.y_pred = self.softmax(self.z3)
+
+        return self.y_pred
     def loss(self, y_pred, y_true):
         pass
     def backprop(self, x, y_true):
